d3b8b03f91a28f4e18649eb620cfb4542be2a1d7
Dan Thackray <djthackray@gmail.com>
2022-12-13 10:20:25 +0000

    Import fetch API and request from National Grid

--- server/index.js
+++ server/index.js
@@ -4,7 +4,27 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.get("/api", (req, res) => {
+const request = import('node-fetch');
+ 
+const url = "https://api.carbonintensity.org.uk/intensity"
+const headers = {
+    'Accept':'application/json'
+};
+ 
+fetch(url,
+    {
+        method: "GET",
+        headers: headers
+    })
+    .then((res) => {
+        return res.json()
+    })
+    .then((body) => {
+        console.log(body)
+    }
+)
+
+app.get("/", (req, res) => {
     res.json({ message: "Hello World!" })
 })
 
